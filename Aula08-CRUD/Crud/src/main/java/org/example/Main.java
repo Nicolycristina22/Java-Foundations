f06455b607b888916da5005f73a604fbb7bc1737
Pedro Henrique Miho de Souza <pedromihosoza@gmail.com>
2025-12-12 16:59:25 -0300

    .

---
+++ Aula08-CRUD/Crud/src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+
+import org.example.entidades.GerenciarAlunos;
+
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) {
+        GerenciarAlunos alunos = new GerenciarAlunos();
+
+        try {
+            alunos.conectar();
+            System.out.println("Conectado com sucesso!");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+}
