facbdb868705071cb73495948abfd14da2eff662
Nicoly Cristina <160439544+Nicolycristina22@users.noreply.github.com>
2025-12-16 16:24:35 -0300

    mnki

--- Aula08-CRUD/Crud/src/main/java/org/example/Main.java
+++ Aula08-CRUD/Crud/src/main/java/org/example/Main.java
@@ -5,15 +5,75 @@ import org.example.entidades.Aluno;
 import org.example.entidades.GerenciarAlunos;
 
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         GerenciarAlunos gerenciarAlunos = new GerenciarAlunos();
 
        Aluno aluno1 = new Aluno("diego", 10,10);
+        Scanner sc = new Scanner(System.in);
+        int opcao;
 
-       //gerenciarAlunos.cadastrarAluno(aluno1);
-       gerenciarAlunos.excluirAluno(1);
+        while(true){
+            System.out.println("menu de opcoes");
+            System.out.println("1 - Cadastrar Aluno");
+            System.out.println("2 - listar Alunos");
+            System.out.println("3 - atualizar Aluno");
+            System.out.println("4 - excluir Aluno");
+            System.out.println("5 - sair");
+            opcao = sc.nextInt();
+            sc.nextLine();
+
+            switch(opcao){
+                case 1:
+                    System.out.println("Digite o nome do aluno:");
+                    String nome = sc.nextLine();
+                    System.out.println("Digite a primeira nota:");
+                    double nota = sc.nextDouble();
+                    System.out.println("Digite a segunda nota:");
+                    double nota2 = sc.nextDouble();
+                    Aluno novoAluno = new Aluno(nome, nota, nota2);
+                    gerenciarAlunos.cadastrarAluno(novoAluno);
+                    break;
+                case 2:
+                    gerenciarAlunos.listarAlunos();
+                    break;
+                case 3:
+                    gerenciarAlunos.listarAlunos();
+                    System.out.println("Digite o ID do aluno a ser atualizado:");
+                    int id = sc.nextInt();
+                    sc.nextLine();
+                    System.out.println("Digite o novo nome do aluno:");
+                    nome = sc.nextLine();
+
+                    System.out.println("Digite a nova primeira nota:");
+                    nota = sc.nextDouble();
+                    sc.nextLine();
+
+                    System.out.println("Digite a nova segunda nota:");
+                    nota2 = sc.nextDouble();
+                    sc.nextLine();
+
+                    gerenciarAlunos.alterarAluno(id, nome, nota, nota2);
+                    break;
+                case 4:
+                    gerenciarAlunos.listarAlunos();
+                    System.out.print("Digite o ID do aluno a ser excluido:");
+                     id = sc.nextInt();
+                    gerenciarAlunos.excluirAluno(id);
+                    break;
+                case 5:
+                    System.out.println("Saindo...");
+                    sc.close();
+                    return;
+                default:
+                    System.out.println("Opcao invalida. Tente novamente.");
+            }
+
+        }
+
+
     }
 
 }
--- Aula08-CRUD/Crud/src/main/java/org/example/entidades/GerenciarAlunos.java
+++ Aula08-CRUD/Crud/src/main/java/org/example/entidades/GerenciarAlunos.java
@@ -1,9 +1,6 @@
 package org.example.entidades;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class GerenciarAlunos {
 
@@ -44,5 +41,45 @@ public class GerenciarAlunos {
      } catch (SQLException e) {
         System.out.println(e.getMessage());
         }
+    }
+    public void alterarAluno(int id, String nome, double nota, double nota2){
+        String sql = "UPDATE alunos SET nome = ?, nota = ?, nota2 = ? WHERE id = ?";
+        try {
+            Connection conexao = conectar();
+            PreparedStatement stmt = conexao.prepareStatement(sql);
+            stmt.setString(1, nome);
+            stmt.setDouble(2, nota);
+            stmt.setDouble(3, nota2);
+            stmt.setInt(4, id);
+            stmt.executeUpdate();
+            stmt.close();
+            System.out.println("Aluno atualizado com sucesso!");
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    public void listarAlunos(){
+        String sql = "SELECT * FROM alunos";
+        try {
+            Connection conexao = conectar();
+
+            PreparedStatement stmt = conexao.prepareStatement(sql);
+             ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()){
+                System.out.println(rs.getInt("id")  + "  "+
+                        rs.getString("nome")  + "  "+
+                        rs.getDouble("nota")  + "  "+
+                        rs.getDouble("nota2")+ "  "+
+                + calcularMedia(rs.getDouble("nota"), rs.getDouble("nota2")));
+            }
+            stmt.close();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    private double calcularMedia(double nota, double nota2){
+        return (nota + nota2) / 2;
     }
 }
